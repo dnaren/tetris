c9bcbb529c1793e17d9911767cf9882ea35a2dcf
narendiran.dorairaj <narendiran@gmail.com>
2020-05-24 06:41:28 -0500

    added different tetromino shapes

--- index.js
+++ index.js
@@ -1,1 +1,51 @@
-document.addEventListener('DOMContentLoaded', () => {});
+document.addEventListener('DOMContentLoaded', () => {
+  const width = 10;
+  const grid = document.querySelector('.grid');
+  const squares = Array.from(document.querySelectorAll('.grid div'));
+  const score = document.querySelector('#score');
+  const btnStart = document.querySelector('#btnStart');
+
+  const ltetrominos = [
+    [0, 1, width, width * 2],
+    [0, 1, 2, width + 2],
+    [1, width + 1, width * 2, width * 2 + 1],
+    [0, width, width + 1, width + 2]
+  ];
+
+  const ztetrominos = [
+    [1, 2, width, width + 1],
+    [0, width, width + 1, width * 2 + 1],
+    [1, 2, width, width + 1],
+    [0, width, width + 1, width * 2 + 1]
+  ];
+
+  const otetrominos = [
+    [0, 1, width, width + 1],
+    [0, 1, width, width + 1],
+    [0, 1, width, width + 1],
+    [0, 1, width, width + 1]
+  ];
+
+  const itetrominos = [
+    [0, width, width * 2, width * 3],
+    [0, 1, 2, 3],
+    [0, width, width * 2, width * 3],
+    [0, 1, 2, 3]
+  ];
+
+  const ttetrominos = [
+    [1, width, width + 1, width + 2],
+    [1, width + 1, width + 2, width * 2 + 1],
+    [0, 1, 2, width + 1],
+    [1, width, width + 1, width * 2 + 1],
+  ];
+
+  let curPosition = 4;
+  paintTetrominos(curPosition, ttetrominos, squares)
+});
+
+function paintTetrominos(curPosition, tetromino, squares) {
+  tetromino[0].forEach(element => {
+    squares[curPosition + element].classList.add('tetromino');
+  });
+}
